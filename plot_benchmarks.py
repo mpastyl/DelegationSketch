56aeb9b1779a5ac24da0c588554dae012fc7b013
Charalampos Stylianopoulos <chasty@ithaca.cse.chalmers.se>
2019-05-02 17:32:51 +0200

    Some results on parallelization and injecting queries

--- plot_benchmarks.py
+++ plot_benchmarks.py
@@ -1,24 +1,51 @@
 from matplotlib import pyplot as plt
 
+def read_perf(filename):
+    returnList = []  
+    with open(filename,'r') as f:
+        for line in f:
+            perf = float(line.split()[3])
+            returnList.append(perf)
+    return returnList
+
 fname_shared_no_q = "shared_no_queries.txt"
 fname_share_nothing_no_q = "share_nothing_no_queries.txt"
 
-shared_no_q = []
-share_nothing_no_q = []
-
-with open(fname_shared_no_q,'r') as f:
-    for line in f:
-        perf = float(line.split()[3])
-        shared_no_q.append(perf)
-
-with open(fname_share_nothing_no_q,'r') as f:
-    for line in f:
-        perf = float(line.split()[3])
-        share_nothing_no_q.append(perf)
+shared_no_q =  read_perf(fname_shared_no_q)
+share_nothing_no_q = read_perf(fname_share_nothing_no_q)
 
 plt.plot(shared_no_q,label='Shared sketch')
 plt.plot(share_nothing_no_q,label="Independent Sketches")
 plt.legend()
 plt.xlabel("Threads")
+plt.ylabel("Mops/sec")
+plt.show()
+
+
+fname_shared_20_threads = "shared_20_threads.txt"
+fname_share_nothing_20_threads = "share_nothing_20_threads.txt"
+
+shared_20_threads = read_perf(fname_shared_20_threads)
+share_nothing_20_threads = read_perf(fname_share_nothing_20_threads)
+
+query_rates = [0,2,4,6,8,10]
+plt.plot(query_rates, shared_20_threads,label='Shared sketch')
+plt.plot(query_rates, share_nothing_20_threads,label="Independent Sketches")
+plt.legend()
+plt.xlabel("Query rate (%)")
+plt.ylabel("Mops/sec")
+plt.show()
+
+fname_shared_10_threads = "shared_10_threads.txt"
+fname_share_nothing_10_threads = "share_nothing_10_threads.txt"
+
+shared_10_threads = read_perf(fname_shared_10_threads)
+share_nothing_10_threads = read_perf(fname_share_nothing_10_threads)
+
+query_rates = [0,2,4,6,8,10]
+plt.plot(query_rates, shared_10_threads,label='Shared sketch')
+plt.plot(query_rates, share_nothing_10_threads,label="Independent Sketches")
+plt.legend()
+plt.xlabel("Query rate (%)")
 plt.ylabel("Mops/sec")
 plt.show()
--- test_configs.sh
+++ test_configs.sh
@@ -14,9 +14,19 @@ sed -i -e "s/REPLACE_FIXED_DURATION/$CONFIG_FIXED_DURATION/g" params.h
 make clean
 make cm_benchmark
 
-for threads in `seq 1 32`
-do
+#rm shared_no_queries.txt
+#rm share_nothing_no_queries.txt
+
+#for threads in `seq 1 32`
+#do
 #./cm_benchmark.out 10000 60000 64 64 1 1 0.5 1 $threads 0 | grep -oP 'Total processing throughput [+-]?[0-9]+([.][0-9]+)?+' -a --text >> shared_no_queries.txt
-./cm_benchmark.out 10000 60000 64 64 1 1 0.5 1 $threads 0 | grep -oP 'Total processing throughput [+-]?[0-9]+([.][0-9]+)?+' -a --text >> share_nothing_no_queries.txt
+#./cm_benchmark.out 10000 60000 64 64 1 1 0.5 1 $threads 0 | grep -oP 'Total processing throughput [+-]?[0-9]+([.][0-9]+)?+' -a --text >> share_nothing_no_queries.txt
+#done
+
+query_rates="0 2 4 6 8 10"
+for queries in $query_rates
+do
+#./cm_benchmark.out 10000 60000 64 64 1 1 0.5 1 10 $queries | grep -oP 'Total processing throughput [+-]?[0-9]+([.][0-9]+)?+' -a --text >> shared_10_threads.txt
+./cm_benchmark.out 10000 60000 64 64 1 1 0.5 1 10 $queries | grep -oP 'Total processing throughput [+-]?[0-9]+([.][0-9]+)?+' -a --text >> share_nothing_10_threads.txt
 done
 mv backup_params.h params.h
