7e6dc87db94b000b619c97d18808876adcc7f1b5
Charalampos Stylianopoulos <chasty@ithaca.cse.chalmers.se>
2019-05-07 13:54:22 +0200

    Added thread pinning for the ithaca server

--- cm_benchmark.cpp
+++ cm_benchmark.cpp
@@ -98,7 +98,9 @@ void * threadEntryPoint(void * threadArgs){
     int tid = *((int *) threadArgs);
     threadDataStruct * localThreadData = &(threadData[tid]);
     //setaffinity_oncpu(14*(tid%2)+(tid/2)%14);
-    setaffinity_oncpu(tid*2);
+    //ITHACA: fill first NUMA node first (even numbers)
+    int thread_id = (tid%36)*2 + tid/36;
+    setaffinity_oncpu(thread_id);
 
     int threadWorkSize = tuples_no /  numberOfThreads;
     localThreadData->startIndex = tid * threadWorkSize;
