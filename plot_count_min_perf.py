759f9dac0a14f5ab323aa18a58d231bf401f0f40
Charalampos Stylianopoulos <mpastyl12@gmail.com>
2019-05-08 17:14:30 +0200

    Added a second accuracy metric and now ploting accuracy of all versions together

--- plot_count_min_perf.py
+++ plot_count_min_perf.py
@@ -1,12 +1,22 @@
 from matplotlib import pyplot as plt
 
-filename = "count_min_results.txt"
+versions = ["shared", "local_copies", "hybrid", "remote_inserts"]
+#filename = "count_min_results.txt"
+threads="20"
 
 def computeARE(groundTruth, estimates):
     sum = 0
     for gt, est in zip(groundTruth,estimates):
         sum += abs(gt - est)/ float(gt)
     return sum / float (len(groundTruth))
+
+def computeObservedError(groundTruth, estimates):
+    sumError = 0
+    sumTrueFreq = 0
+    for gt, est in zip(groundTruth,estimates):
+        sumError += abs(gt - est)
+        sumTrueFreq += gt
+    return float(sumError)/sumTrueFreq
 
 def deserializeFrequencies(indexes,hist,answers):
     returnTrueFreqs = []
@@ -16,26 +26,40 @@ def deserializeFrequencies(indexes,hist,answers):
         returnEstimates += [answers[index]] * hist[index]
     return returnTrueFreqs, returnEstimates
 
-hist = []
-answers = []
-indexes = []
+totalHist = {}
+totalAnswers = {}
+for version in versions:
+    filename = "cm_"+version+"_"+threads+"_accuracy.log"
+    hist = []
+    answers = []
+    indexes = []
 
-with open(filename) as fp:
-    for line in fp:
-        index = int(line.split()[0])
-        trueValue = int(line.split()[1])
-        approximation =  float(line.split()[2])
-        
-        indexes.append(index)
-        hist.append(trueValue)
-        answers.append(approximation)
+    with open(filename) as fp:
+        for line in fp:
+            index = int(line.split()[0])
+            trueValue = int(line.split()[1])
+            approximation =  float(line.split()[2])
 
-hist, answers = deserializeFrequencies(indexes, hist, answers)
+            indexes.append(index)
+            hist.append(trueValue)
+            answers.append(approximation)
 
-print "Average Relative Error: ", computeARE(hist,answers)
-print "Absolute difference on most frequent element: ", answers[0] - hist[0]
+    hist, answers = deserializeFrequencies(indexes, hist, answers)
 
-plt.plot(answers, label = "Approximation")
-plt.plot(hist,label = "True")
+    print "Version ", version, " Average Relative Error: ", computeARE(hist,answers)
+    print "Version ", version, " Observed Error: ", computeObservedError(hist, answers)
+    #print "Absolute difference on most frequent element: ", answers[0] - hist[0]
+
+    # plt.plot(answers, label = "Approximation")
+    # plt.plot(hist,label = "True")
+    # plt.legend()
+    # plt.show()
+    
+    totalHist[version] = hist
+    totalAnswers[version] = answers
+
+plt.plot(totalHist[versions[0]], label = "True")
+for version in versions:
+    plt.plot(totalAnswers[version], label = version)
 plt.legend()
 plt.show()
--- test_configs.sh
+++ test_configs.sh
@@ -4,27 +4,43 @@ make all
 versions="cm_shared cm_local_copies cm_hybrid cm_remote_inserts"
 
 
-for version in $versions
-do
-    echo $version
-    rm -f ${version}_no_queries.log
-    for threads in `seq 1 28`
-    do
-        ./$version.out 10000 60000 64 64 1 1 1 1 $threads 0 1 | grep -oP 'Total processing throughput [+-]?[0-9]+([.][0-9]+)?+' -a --text >> ${version}_no_queries.log
-    done
-done
-
-query_rates="0 2 4 6 8 10"
 thread_list="20"
+query_rates="0"
 for version in $versions
 do
     for threads  in $thread_list
     do
         echo ${version} ${threads}
-        rm -f ${version}_${threads}_threads.log
+        rm -f ${version}_${threads}_accuracy.log
         for queries in $query_rates
         do
-            ./$version.out 10000 60000 64 64 1 1 1 1 $threads $queries 1 | grep -oP 'Total processing throughput [+-]?[0-9]+([.][0-9]+)?+' -a --text >> ${version}_${threads}_threads.log
+            ./$version.out 10000 60000 64 64 1 1 0 1 $threads $queries 0 >/dev/null
+            cp count_min_results.txt ${version}_${threads}_accuracy.log
         done
     done
 done
+
+# for version in $versions
+# do
+#     echo $version
+#     rm -f ${version}_no_queries.log
+#     for threads in `seq 1 28`
+#     do
+#         ./$version.out 10000 60000 64 64 1 1 0 1 $threads 0 1 | grep -oP 'Total processing throughput [+-]?[0-9]+([.][0-9]+)?+' -a --text >> ${version}_no_queries.log
+#     done
+# done
+
+# query_rates="0 2 4 6 8 10"
+# thread_list="20"
+# for version in $versions
+# do
+#     for threads  in $thread_list
+#     do
+#         echo ${version} ${threads}
+#         rm -f ${version}_${threads}_threads.log
+#         for queries in $query_rates
+#         do
+#             ./$version.out 10000 60000 64 64 1 1 0 1 $threads $queries 1 | grep -oP 'Total processing throughput [+-]?[0-9]+([.][0-9]+)?+' -a --text >> ${version}_${threads}_threads.log
+#         done
+#     done
+# done
