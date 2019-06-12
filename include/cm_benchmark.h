74f739b33c2fc1c7f94652016e8506e8e5532583
Charalampos Stylianopoulos <mpastyl12@gmail.com>
2019-06-12 18:22:00 +0200

    Now I use default values for unused preprocessor flags

--- include/cm_benchmark.h
+++ include/cm_benchmark.h
@@ -1,6 +1,32 @@
 #ifndef CM_BENCHMARK_H
 #define CM_BENCHMARK_H
 
+
+//define preprocessor flags that are not set in the Makefile
+#ifndef SHARED_SKETCH
+#define SHARED_SKETCH 0
+#endif
+
+#ifndef LOCAL_COPIES
+#define LOCAL_COPIES 0
+#endif
+
+#ifndef HYBRID
+#define HYBRID 0
+#endif
+
+#ifndef REMOTE_INSERTS
+#define REMOTE_INSERTS 0
+#endif
+
+#ifndef USE_MPSC
+#define USE_MPSC 0
+#endif
+
+#ifndef USE_FILTER
+#define USE_FILTER 0
+#endif
+
 int QUERRY_RATE;
 int DURATION;
 void * threadEntryPoint(void * threadArgs);
