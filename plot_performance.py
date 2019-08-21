1b96a524db481d20af2543f15c5a67eae8b63a72
Charalampos Stylianopoulos <mpastyl12@gmail.com>
2019-08-21 16:55:56 +0200

    Added a lot of cosmetic changes on the performance plots

--- plot_performance.py
+++ plot_performance.py
@@ -1,12 +1,16 @@
 from matplotlib import pyplot as plt
 import math
 import subprocess
+from matplotlib.font_manager import FontProperties
+
+fontP = FontProperties()
+fontP.set_size('medium')
 
 from itertools import cycle
 lines = ["-","--","-.",":"]
 linecycler = cycle(lines)
 
-threads = range(1,73,4)
+threads = range(1,289,10)
 query_rates = [0,1,2,3]
 #versions = ["shared", "local_copies", "hybrid", "remote_inserts", "remote_inserts_filtered", "shared_filtered", "local_copies_filtered", "augmented_sketch", "delegation_filters", "delegation_filters_with_linked_list"] 
 versions = ["shared", "local_copies", "augmented_sketch", "delegation_filters", "delegation_filters_with_linked_list"] 
@@ -27,7 +31,7 @@ for version in versions:
 
 rows = int(math.sqrt(len(query_rates)))
 columns = len(query_rates)/rows
-fig,ax = plt.subplots(rows, columns,squeeze=False)
+fig,ax = plt.subplots(rows, columns, figsize=(20,15))
 serial_ax = []
 for row in ax:
     for col in row:
@@ -35,22 +39,23 @@ for row in ax:
 
 plot_count = 0
 for queries in query_rates:
+    linecycler = cycle(lines)
     for version in versions:
         serial_ax[plot_count].plot(threads,ScalingData[(version,queries)], next(linecycler), label = version+" - 0."+str(queries)+"%")
-        serial_ax[plot_count].legend(loc=2)
+        #serial_ax[plot_count].legend(loc=2)
+        serial_ax[plot_count].legend(bbox_to_anchor=(0,1.02,1,0.2), loc="lower left", mode="expand", borderaxespad=0, ncol=2, prop=fontP)
         serial_ax[plot_count].set_xlabel("Threads")
         serial_ax[plot_count].set_ylabel("Mops/sec")
         #serial_ax[plot_count].set_ylim(0,70)
     
     plot_count +=1
-#plt.xlabel("Threads")
-#plt.ylabel("Mops/sec")
 name="/home/chasty/sketches/rusu-sketches-size-join-estimation/scaling_at_1.5_skew.pdf"
 plt.savefig(name)
 plt.show()
+#subprocess.Popen("/home/chasty/pdfcrop_v0.4b/pdfcrop "+name.split('/')[-1]+" "+name.split('/')[-1],shell=True)
 
 query_rates = [0,1,2,3,4,5,6]
-threads="72"
+threads="288"
 QueriesData = {}
 for version in versions:
     QueriesData[version] = read_perf("logs/cm_"+version+"_"+threads+"_threads.log")
@@ -58,16 +63,22 @@ for version in versions:
 for version in versions:
     #plt.plot(query_rates,QueriesData[version],  next(linecycler), label = version)
     plt.plot([float(x)/10 for x in query_rates], QueriesData[version],  next(linecycler), label = version)
-lgd=plt.legend()
+#lgd=plt.legend()
+#lgd = plt.legend(bbox_to_anchor=(0,0.8), loc="lower left", ncol = 3)
+lgd = plt.legend(bbox_to_anchor=(0,1.02,1,0.2), loc="lower left", mode="expand", borderaxespad=0, ncol=3, prop=fontP)
+#lgd = plt.legend(bbox_to_anchor=(0,0), loc="upper left", ncol = 3)
 plt.xlabel("Query rate (%)")
 plt.ylabel("Mops/sec")
 name="/home/chasty/sketches/rusu-sketches-size-join-estimation/query_effect_1.5_skew.pdf"
 plt.savefig(name,bbox_extra_artists=(lgd,), bbox_inches = "tight")
 plt.show()
+#subprocess.Popen("/home/chasty/pdfcrop_v0.4b/pdfcrop "+name.split('/')[-1]+" "+name.split('/')[-1],shell=True)
 
+
+FIG_SIZE = (16,10)
 skew_rates_raw_list = "0 0.25 0.5 0.75 1 1.25 1.5 1.75 2 2.25 2.5 2.75 3 3.25 3.5 3.75 4"
 skew_rates = [float(x) for x in skew_rates_raw_list.split()]
-threads="72"
+threads="288"
 SkewnesData = {}
 query_rates = [0,1,2,3]
 for version in versions:
@@ -75,13 +86,15 @@ for version in versions:
             SkewnesData[(version, query)] = read_perf("logs/skew_cm_"+version+"_"+threads+"_threads_"+str(query)+"_queries.log")
 
 for query in query_rates:
+    linecycler = cycle(lines)
+    plt.figure(figsize=FIG_SIZE)
     for version in versions:
         plt.plot(skew_rates,SkewnesData[(version,query)], next(linecycler), label = version + " 0."+ str(query)+ "% queries")
-    lgd = plt.legend()
+    #lgd = plt.legend()
+    lgd = plt.legend(bbox_to_anchor=(0,1.02,1,0.2), loc="lower left", mode="expand", borderaxespad=0, ncol=2)
     plt.xlabel("Skew parameter")
     plt.ylabel("Mops/sec")
     name="/home/chasty/sketches/rusu-sketches-size-join-estimation/skew_"+str(query)+"_queries.pdf"
     plt.savefig(name,bbox_extra_artists=(lgd,), bbox_inches = "tight")
-    #subprocess.Popen("pdfcrop "+name+" "+name,shell=True)
-    #subprocess.Popen("pdfcrop")
+    #subprocess.Popen("/home/chasty/pdfcrop_v0.4b/pdfcrop "+name.split('/')[-1]+" "+name.split('/')[-1],shell=True)
     plt.show()
