a9ac323ad62c5d3794281d5193c514cfa944f185
Charalampos Stylianopoulos <mpastyl12@gmail.com>
2019-09-27 10:40:06 +0200

    Changes on accuracy experiments. Double check domain sizes and stream sizes before running again

--- plot_accuracy.py
+++ plot_accuracy.py
@@ -4,11 +4,20 @@ from itertools import cycle
 lines = ["-","--","-.",":"]
 linecycler = cycle(lines)
 
+from matplotlib.font_manager import FontProperties
+fontP = FontProperties()
+fontP.set_size('large')
+
+suffix="zipf"
+
 #versions = ["shared", "local_copies", "hybrid", "remote_inserts", "remote_inserts_filtered", "shared_filtered", "local_copies_filtered", "augmented_sketch", "delegation_filters", "delegation_filters_with_linked_list"]
-versions = ["shared", "local_copies", "augmented_sketch", "delegation_filters", "delegation_filters_with_linked_list"]
+#versions = ["shared", "local_copies", "augmented_sketch", "delegation_filters", "delegation_filters_with_linked_list"]
+versions = ["local_copies", "shared", "remote_inserts","shared_small"]
+fancy_names = ["Thread-local", "Single Sketch", "Domain-spliting", "Reference"]
 #filename = "count_min_results.txt"
 #thread_list=range(1,29)
-thread_list=[69]
+#thread_list=[10]
+thread_list=range(1,11)
 
 def sortAccordingToTrueValues(trueValues, answers):
     #zipped = zip(trueValues,answers)
@@ -47,6 +56,9 @@ def deserializeFrequencies(indexes,hist,answers):
 
 totalHist = {}
 totalAnswers = {}
+
+totalARE={}
+totalObsE={}
 for version in versions:
     for threads in thread_list:
         filename = "logs/cm_"+version+"_"+str(threads)+"_accuracy.log"
@@ -66,8 +78,10 @@ for version in versions:
 
         hist, answers = deserializeFrequencies(indexes, hist, answers)
 
-        print "Version ", version, " Average Relative Error: ", computeARE(hist,answers)
-        print "Version ", version, " Observed Error: ", computeObservedError(hist, answers)
+        totalARE[(version, threads)] = computeARE(hist,answers) 
+        totalObsE[(version, threads)] = computeObservedError(hist, answers)
+        print "Version ", version, " Average Relative Error: ", totalARE[(version, threads)] 
+        print "Version ", version, " Observed Error: ", totalObsE[(version, threads)]
 
         totalHist[(version,threads)] = hist
         totalAnswers[(version,threads)] = answers
@@ -85,20 +99,29 @@ for version in versions:
         if totalAnswers[(version,thread_list[0])][i] < totalHist[(versions[0],thread_list[0])][i]:
             print "Under estimation in ",i
 
-# realValues = totalHist[(versions[0],thread_list[0])]
-# for version in versions:
-#     ARE = [computeARE(realValues, totalAnswers[(version,threads)]) for threads in thread_list]
-#     plt.plot(ARE, label = version)
-# plt.xlabel("Threads")
-# plt.ylabel("Average Relative Error")
-# plt.legend(loc=2)
-# plt.show()
+FIG_SIZE = (5,5)
+plt.figure(figsize=FIG_SIZE)
+c = 0
+for version in versions:
+    ARE = [totalARE[(version,threads)] for threads in thread_list]
+    plt.plot(thread_list, ARE, label = fancy_names[c])
+    c = c +1
+plt.xlabel("Threads")
+plt.ylabel("Average Relative Error")
+lgd = plt.legend(loc = 'center right')
+name="/home/chasty/sketches/rusu-sketches-size-join-estimation/average_relative_error_"+suffix+".pdf"
+plt.savefig(name, bbox_extra_artists=(lgd,), bbox_inches = "tight")
+plt.show()
 
-# realValues = totalHist[(versions[0],thread_list[0])]
-# for version in versions:
-#     ObservedError = [computeObservedError(realValues, totalAnswers[(version,threads)]) for threads in thread_list]
-#     plt.plot(ObservedError, label = version)
-# plt.xlabel("Threads")
-# plt.ylabel("Observed Error")
-# plt.legend(loc=2)
-# plt.show()
+plt.figure(figsize=FIG_SIZE)
+c = 0
+for version in versions:
+    ObservedError = [ totalObsE[(version, threads)] for threads in thread_list]
+    plt.plot(thread_list, ObservedError, label = fancy_names[c])
+    c = c +1
+plt.xlabel("Threads")
+plt.ylabel("Observed Error (%)")
+lgd = plt.legend(loc = 'center right')
+name="/home/chasty/sketches/rusu-sketches-size-join-estimation/observed_error_"+suffix+".pdf"
+plt.savefig(name, bbox_extra_artists=(lgd,), bbox_inches = "tight")
+plt.show()
--- test_accuracy.sh
+++ test_accuracy.sh
@@ -1,14 +1,18 @@
-RUN_ON_ITHACA=0
-thread_list=`seq 1 4 72`
+RUN_ON_ITHACA=2
+#thread_list=`seq 1 10 288`
+thread_list=`seq 1 1 10`
 
 buckets=256
 rows=8
+skew=1
 
-universe_size=1000000
-stream_size=6000000
+universe_size=100000
+stream_size=600000
 
 #ersions="cm_shared cm_local_copies cm_hybrid cm_remote_inserts cm_remote_inserts_filtered cm_shared_filtered cm_local_copies_filtered cm_augmented_sketch cm_delegation_filters cm_delegation_filters_with_linked_list"
-versions="cm_shared cm_local_copies cm_local_copies cm_augmented_sketch cm_delegation_filters cm_delegation_filters_with_linked_list"
+#versions="cm_shared cm_local_copies cm_local_copies cm_augmented_sketch cm_delegation_filters cm_delegation_filters_with_linked_list"
+versions="cm_shared cm_local_copies cm_remote_inserts cm_shared_small"
+#versions="cm_remote_inserts"
 
 cd src
 make clean
@@ -26,15 +30,18 @@ do
         do
             if [ "$version" = "cm_shared" ] || [ "$version" = "cm_shared_filtered" ]; then
             	echo "Memory per thread" $(($buckets*$rows*4))
-                ./bin/$version.out $universe_size $stream_size $(($buckets*$threads)) $rows 1 1 0 1 $threads $queries 0 >/dev/null
+                ./bin/$version.out $universe_size $stream_size $(($buckets*$threads)) $rows 1 $skew 0 1 $threads $queries 0 >/dev/null
             elif [ "$version" = "cm_delegation_filters" ] || [ "$version" = "cm_delegation_filters_with_linked_list" ]; then
-	    	new_columns=$((($buckets*$rows*4 - $threads*64)/($rows*4)))
-	    	echo "Memory per thread " $(($new_columns*$rows*4 + $threads*64))
-	    	echo "Using " $new_columns "columns"
-                ./bin/$version.out $universe_size $stream_size $new_columns $rows 1 1 0 1 $threads $queries 0 >/dev/null
+	    	    new_columns=$((($buckets*$rows*4 - $threads*64)/($rows*4)))
+	    	    echo "Memory per thread " $(($new_columns*$rows*4 + $threads*64))
+	    	    echo "Using " $new_columns "columns"
+                ./bin/$version.out $universe_size $stream_size $new_columns $rows 1 $skew 0 1 $threads $queries 0 >/dev/null
+            elif [ "$version" = "cm_shared_small" ]; then
+		        echo "Memory (total)" $(($buckets*$rows*4))
+                ./bin/cm_shared.out $universe_size $stream_size $buckets $rows 1 $skew 0 1 $threads $queries 0 >/dev/null
             else
-		echo "Memory per thread" $(($buckets*$rows*4))
-                ./bin/$version.out $universe_size $stream_size $buckets $rows 1 1 0 1 $threads $queries 0 >/dev/null
+		        echo "Memory per thread" $(($buckets*$rows*4))
+                ./bin/$version.out $universe_size $stream_size $buckets $rows 1 $skew 0 1 $threads $queries 0 >/dev/null
             fi
             cp logs/count_min_results.txt logs/${version}_${threads}_accuracy.log
         done
